5c39f780828c39733b6101181181980db19b25c5
Rachid <rachid.leftouhi@gmail.com>
2025-02-11 09:33:39 +0100

    Connexion html et Login controller

---
+++ src/main/java/fr/eni/encheres/configuration/SecurityConfig.java
@@ -0,0 +1,61 @@
+package fr.eni.encheres.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((authorize) -> authorize
+				.requestMatchers("/*").permitAll()
+				.requestMatchers("/login").permitAll()
+				.anyRequest().denyAll()//interdit l'accès aux urls non configurées
+			)
+			.httpBasic(Customizer.withDefaults())
+			.formLogin((formLogin) ->
+				formLogin
+					.loginPage("/login")
+					.defaultSuccessUrl("/")
+			)
+			.logout((logout) ->
+				logout
+					.invalidateHttpSession(true)
+					.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+					.logoutSuccessUrl("/")
+			);	
+			
+
+		return http.build();
+	}
+	
+	
+	@Bean
+	//Permet de chercher les utilisateurs en base de donnée
+	UserDetailsManager users(DataSource dataSource) {
+		
+		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+		jdbcUserDetailsManager.setUsersByUsernameQuery("SELECT pseudo, mot_de_passe, 'true' AS enabled FROM UTILISATEURS WHERE pseudo = ? OR email = ?");
+		
+		return jdbcUserDetailsManager;
+	}
+}
