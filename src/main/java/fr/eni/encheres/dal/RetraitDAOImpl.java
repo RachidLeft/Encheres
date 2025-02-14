5a07434a54751b0272fd8126ce37307ba48243bb
Rachid <rachid.leftouhi@gmail.com>
2025-02-14 10:38:10 +0100

    Methode read pour Retrait avec RowMapper

--- src/main/java/fr/eni/encheres/dal/RetraitDAOImpl.java
+++ src/main/java/fr/eni/encheres/dal/RetraitDAOImpl.java
@@ -1,13 +1,19 @@
 package fr.eni.encheres.dal;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import fr.eni.encheres.bo.ArticleVendu;
+import fr.eni.encheres.bo.Retrait;
 
 public class RetraitDAOImpl implements RetraitDAO{
 	
 	private static final String INSERT = "INSERT INTO RETRAITS no_article, rue, code_postal, ville values :noArticle, :rue, :codePostal, :ville";
+	private static final String SELECT_BY_ID ="SELECT no_article, rue, code_postal, ville FROM RETRAITS WHERE no_article = :noArticle";
 	
 private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 	
@@ -27,4 +33,31 @@ private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 		
 	}
 
+	@Override
+	public Retrait read(int noArticle) {
+		
+		MapSqlParameterSource map = new MapSqlParameterSource();
+		map.addValue("noArticle", noArticle);
+		return namedParameterJdbcTemplate.queryForObject(SELECT_BY_ID, map, new RetraitRowMapper());
+	}
+	
+	
+	class RetraitRowMapper implements RowMapper<Retrait> {
+
+		@Override
+		public Retrait mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+			Retrait r = new Retrait();
+			r.setRue(rs.getString("rue"));
+			r.setVille(rs.getString("ville"));
+			r.setCodePostal(rs.getString("code_postal"));
+			
+			// Association pour l'article vendu
+			ArticleVendu a = new ArticleVendu();
+			a.setNoArticle(rs.getInt("no_article"));
+			r.setArticle(a);
+			return r;
+		}
+
+	}
 }
